3b8bf6b6c39c8bc2d98d1b2e597b6130eafb8a94
claypeacock <claypeacock@gmail.com>
2024-06-14 16:06:05 -0400

    Update streamlit_app.py
    added API to bring in requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success("Your Smoothie is ordered, "+name_on_smoothie+"!", icon="✅")
+
+# API WORK
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
